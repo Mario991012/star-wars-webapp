9c69936d422bf5a908240fee73af077901f9fe8d
Mario Velasquez <mvelasquezb99@gmail.com>
2024-09-02 04:21:46 -0600

    feat: Sidebar added. Layouts updated.

--- src/app/presentation/layouts/authorized-layout/authorized-layout.component.ts
+++ src/app/presentation/layouts/authorized-layout/authorized-layout.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-authorized-layout',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, RouterOutlet],
   templateUrl: './authorized-layout.component.html',
   styleUrl: './authorized-layout.component.scss'
 })
--- src/app/presentation/layouts/unauthorized-layout/unauthorized-layout.component.ts
+++ src/app/presentation/layouts/unauthorized-layout/unauthorized-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-unauthorized-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './unauthorized-layout.component.html',
   styleUrl: './unauthorized-layout.component.scss'
 })
