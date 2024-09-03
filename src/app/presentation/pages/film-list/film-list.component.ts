2cba7e5bd9048e207292aed5649458f10cb327dd
Mario Velasquez <mvelasquezb99@gmail.com>
2024-09-03 16:46:46 -0600

    feat: filter added to film list. filter by title, director and producers. tests added.

--- src/app/presentation/pages/film-list/film-list.component.ts
+++ src/app/presentation/pages/film-list/film-list.component.ts
@@ -15,10 +15,17 @@ import { FilmDetailsDialogComponent } from '../../components/dialogs/film-detail
 import { Film } from '../../../data/graphql/interfaces/film.interface';
 import { ICharacter } from '../../../data/graphql/interfaces/character.interface';
 import { Planet } from '../../../data/graphql/interfaces/planet.interface';
+import { IFilterForm } from '../../interfaces/filter-form.interface';
+import { FilterFormComponent } from '../../components/filters/filter-form/filter-form.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, FilmCardComponent, MatDialogModule],
+  imports: [
+    CommonModule,
+    FilmCardComponent,
+    MatDialogModule,
+    FilterFormComponent,
+  ],
   selector: 'app-film-list',
   templateUrl: './film-list.component.html',
   styleUrls: ['./film-list.component.scss'],
@@ -26,10 +33,17 @@ import { Planet } from '../../../data/graphql/interfaces/planet.interface';
 })
 export class FilmListComponent implements OnInit, OnDestroy {
   films$!: Observable<Film[]>;
+  filteredFilms$!: Observable<Film[]>;
   error$!: Observable<string>;
   private destroy$ = new Subject<void>();
   private filmService = inject(FilmService);
   readonly dialog = inject(MatDialog);
+
+  filterFields: IFilterForm[] = [
+    { label: 'Director', formControlName: 'director' },
+    { label: 'Producers', formControlName: 'producers' },
+    { label: 'Title', formControlName: 'title' },
+  ];
 
   ngOnInit(): void {
     this.getFilmsMetadata();
@@ -61,14 +75,37 @@ export class FilmListComponent implements OnInit, OnDestroy {
       map((result) => result || []),
       catchError((error) => {
         console.error('Error fetching film list:', error);
-        return [
-          error.message || 'An error occurred while fetching the film list.',
-        ];
+        return [];
       })
     );
 
+    this.filteredFilms$ = this.films$;
+
     this.error$ = this.films$.pipe(
-      map((films) => (Array.isArray(films) ? '' : films))
+      map((films) => (films.length ? '' : 'No films found'))
+    );
+  }
+
+  onFilterChanged(filters: any): void {
+    this.filteredFilms$ = this.films$.pipe(
+      map((films) =>
+        films.filter(
+          (film) =>
+            (!filters.director ||
+              film.director
+                ?.toLocaleLowerCase()
+                .includes(filters.director.toLocaleLowerCase())) &&
+            (!filters.producers ||
+              film.producers
+                .join(' ')
+                .toLocaleLowerCase()
+                .includes(filters.producers.toLocaleLowerCase())) &&
+            (!filters.title ||
+              film.title
+                ?.toLocaleLowerCase()
+                .includes(filters.title.toLocaleLowerCase()))
+        )
+      )
     );
   }
 }
