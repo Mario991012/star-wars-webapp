a009102de716898024d7cac861b59cb26a7d29c2
Mario Velasquez <mvelasquezb99@gmail.com>
2024-09-02 23:12:38 -0600

    feat: added comments to jwt in environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   graphqlEndpoint: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
+  //! This is just an example. JWT should not be here.
   mockToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoic3Rhci53YXJzQGVtYWlsLmNvbSIsInBhc3N3b3JkIjoicGFzc3dvcmQxMjMiLCJpYXQiOjE1MTYyMzkwMjJ9.wp5JNgz4-RwZDoYwzaBZ8CPTFGTp7MMEL19iH-l95G8',
 };
