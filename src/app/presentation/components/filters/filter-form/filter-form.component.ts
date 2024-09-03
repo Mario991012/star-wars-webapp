bb93d31426ed43a699f3ae0aa5b1ee77cb2af9c7
Mario Velasquez <mvelasquezb99@gmail.com>
2024-09-03 15:32:28 -0600

    test: Added id to each input generated. Id used in tests to locate the new inputs. Tests added.

--- src/app/presentation/components/filters/filter-form/filter-form.component.ts
+++ src/app/presentation/components/filters/filter-form/filter-form.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  OnDestroy,
+} from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -9,6 +16,8 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { IFilterForm } from '../../../interfaces/filter-form.interface';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-filter-form',
@@ -23,24 +32,38 @@ import { IFilterForm } from '../../../interfaces/filter-form.interface';
   templateUrl: './filter-form.component.html',
   styleUrls: ['./filter-form.component.scss'],
 })
-export class FilterFormComponent implements OnInit {
+export class FilterFormComponent implements OnInit, OnDestroy {
   @Input() fields: IFilterForm[] = [];
   filterForm!: FormGroup;
 
-  @Output() filterChanged = new EventEmitter<any>();
+  @Output() filterChanged = new EventEmitter<Record<string, any>>();
+
+  private destroy$ = new Subject<void>();
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initializeForm();
+    this.setupValueChangesSubscription();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private initializeForm(): void {
     const formControls = this.fields.reduce((controls, field) => {
       controls[field.formControlName] = [''];
       return controls;
-    }, {} as { [key: string]: any });
+    }, {} as Record<string, any>);
 
     this.filterForm = this.fb.group(formControls);
+  }
 
-    this.filterForm.valueChanges.subscribe((value) => {
-      this.filterChanged.emit(value);
-    });
+  private setupValueChangesSubscription(): void {
+    this.filterForm.valueChanges
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((value) => this.filterChanged.emit(value));
   }
 }
