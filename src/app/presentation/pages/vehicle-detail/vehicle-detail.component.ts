7ad9a4323bc0ae553d3bec16db257058c212d6e2
Mario Velasquez <mvelasquezb99@gmail.com>
2024-09-04 02:46:36 -0600

    fix: Error handling added in vehicle detail. scss updated

--- src/app/presentation/pages/vehicle-detail/vehicle-detail.component.ts
+++ src/app/presentation/pages/vehicle-detail/vehicle-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { VehicleService } from '../../../data/graphql/services/vehicle.service';
-import { catchError, map, Observable, Subject, takeUntil } from 'rxjs';
+import { catchError, map, Observable, of, Subject, takeUntil } from 'rxjs';
 import { IVehicle } from '../../../data/graphql/interfaces/vehicle.interface';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -57,7 +57,7 @@ export class VehicleDetailComponent implements OnInit, OnDestroy {
    */
   async ngOnInit(): Promise<void> {
     this.route.paramMap.subscribe(async params => {
-      this.loading = true;
+      this.error$ = of("Loading...");
       const id: string = params.get('id') || "";
       await this.getVehicleData(id);
       this.loading = false;
@@ -79,11 +79,13 @@ export class VehicleDetailComponent implements OnInit, OnDestroy {
    * @param id - The ID of the vehicle to fetch data for.
    */
   private getVehicleData(id: string): void {
+    this.error$ = of("");
     this.vehicle$ = this.vehicleService.getVehicleById(id).pipe(
       takeUntil(this.destroy$),
       map((result) => result || []),
       catchError((error) => {
-        console.error('Error fetching vehicle data:', error);
+        console.error('Error fetching character data:', error);
+        this.error$ = of('Something went wrong while getting the character. Reload the page and try again.');
         return [];
       })
     );
