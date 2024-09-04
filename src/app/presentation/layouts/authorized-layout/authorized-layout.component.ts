709f17a881a78809fc23279b885151a0431ff4b6
Mario Velasquez <mvelasquezb99@gmail.com>
2024-09-03 21:37:39 -0600

    feat: sidebar behaviour improved. Hides in mobile and shows in desktop. Added blurred background when sidebar is active.

--- src/app/presentation/layouts/authorized-layout/authorized-layout.component.ts
+++ src/app/presentation/layouts/authorized-layout/authorized-layout.component.ts
@@ -1,51 +1,70 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { Router, RouterOutlet } from '@angular/router';
 import { RouteData } from '../../interfaces/route-data.interface';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-authorized-layout',
   standalone: true,
-  imports: [SidebarComponent, RouterOutlet],
+  imports: [
+    CommonModule,
+    SidebarComponent,
+    RouterOutlet,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './authorized-layout.component.html',
-  styleUrl: './authorized-layout.component.scss'
+  styleUrls: ['./authorized-layout.component.scss'],
 })
 export class AuthorizedLayoutComponent {
+  isSidebarOpen = false;
 
   routes: RouteData[] = [
     {
-      route: "/star-wars/film-list",
-      title: "Films",
-      icon: "dashboard",
+      route: '/star-wars/film-list',
+      title: 'Films',
+      icon: 'dashboard',
       selected: false,
     },
     {
-      route: "/star-wars/people-list",
-      title: "People",
-      icon: "people",
+      route: '/star-wars/people-list',
+      title: 'People',
+      icon: 'people',
       selected: false,
     },
     {
-      route: "/star-wars/vehicles/vehicle-list",
-      title: "Vehicles",
-      icon: "commute",
+      route: '/star-wars/vehicles/vehicle-list',
+      title: 'Vehicles',
+      icon: 'commute',
       selected: false,
     },
     {
-      route: "/star-wars/not-found",
-      title: "Not Found",
-      icon: "public",
+      route: '/star-wars/not-found',
+      title: 'Not Found',
+      icon: 'public',
       selected: false,
     },
-  ]
+  ];
 
-  constructor( private router: Router ) {}
+  constructor(private router: Router) {}
 
-  goToRoute( route: string ): void {
-    this.routes = this.routes.map( actualRoute => ({
+  toggleSidebar(): void {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  closeSidebar(): void {
+    this.isSidebarOpen = false;
+  }
+
+  goToRoute(route: string): void {
+    this.routes = this.routes.map((actualRoute) => ({
       ...actualRoute,
       selected: actualRoute.route === route,
-    }))
-    this.router.navigate([ route ]);
+    }));
+    this.router.navigate([route]);
+    this.closeSidebar();
   }
 }
