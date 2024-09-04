7e8542f94cc66a43a8b19aa831c4c90679e4a1ec
Mario Velasquez <mvelasquezb99@gmail.com>
2024-09-03 22:20:16 -0600

    improvement: Router with lazy loading of standalone components. Login clears the token in local storage.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,9 @@
 import { Routes } from '@angular/router';
-import { FilmListComponent } from './presentation/pages/film-list/film-list.component';
 import { UnauthorizedLayoutComponent } from './presentation/layouts/unauthorized-layout/unauthorized-layout.component';
 import { LoginComponent } from './presentation/pages/login/login.component';
 import { AuthorizedLayoutComponent } from './presentation/layouts/authorized-layout/authorized-layout.component';
 import { NotFoundComponent } from './presentation/pages/not-found/not-found.component';
 import { AuthGuard } from './core/guards/auth/auth.guard';
-import { PeopleListComponent } from './presentation/pages/people-list/people-list.component';
-import { VehicleListComponent } from './presentation/pages/vehicle-list/vehicle-list.component';
-import { VehicleDetailComponent } from './presentation/pages/vehicle-detail/vehicle-detail.component';
 
 export const routes: Routes = [
   {
@@ -17,13 +13,25 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: 'film-list', pathMatch: 'full' },
       { path: 'home', redirectTo: 'film-list', pathMatch: 'full' },
-      { path: 'film-list', component: FilmListComponent },
-      { path: 'people-list', component: PeopleListComponent },
+      { 
+        path: 'film-list', 
+        loadComponent: () => import('./presentation/pages/film-list/film-list.component').then(m => m.FilmListComponent)
+      },
+      { 
+        path: 'people-list', 
+        loadComponent: () => import('./presentation/pages/people-list/people-list.component').then(m => m.PeopleListComponent)
+      },
       {
         path: 'vehicles',
         children: [
-          { path: 'vehicle-list', component: VehicleListComponent },
-          { path: 'details/:id', component: VehicleDetailComponent },
+          { 
+            path: 'vehicle-list', 
+            loadComponent: () => import('./presentation/pages/vehicle-list/vehicle-list.component').then(m => m.VehicleListComponent)
+          },
+          { 
+            path: 'details/:id', 
+            loadComponent: () => import('./presentation/pages/vehicle-detail/vehicle-detail.component').then(m => m.VehicleDetailComponent)
+          },
         ],
       },
       { path: '**', component: NotFoundComponent },
--- src/app/presentation/pages/login/login.component.ts
+++ src/app/presentation/pages/login/login.component.ts
@@ -43,6 +43,8 @@ export class LoginComponent implements OnDestroy {
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]],
     });
+
+    this.loginService.clearToken();
   }
 
   get email() {
